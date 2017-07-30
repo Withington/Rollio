3d56f9c87968ad7d7ec1a48935889c3501662561
Withington <lucy.withington@outlook.com>
2017-07-30 18:22:32 +0100

    Refining Travis neo4j set up.

--- flaskr/test_neo4j.py
+++ flaskr/test_neo4j.py
@@ -18,24 +18,20 @@ class Neo4jTestCase(unittest.TestCase):
 
         
     def test_neo4j(self):
-        # Password handling taken from https://github.com/robinedwards/django-neomodel
         
         local = False
-        if local:
-            password = "lucy"
-        else:
-            password = "neo4j"
+        password = "lucy" if local else "neo4j"
         
         # encrypted set to false due to Travis SSL bug Jul17 - https://github.com/neo4j/neo4j/issues/9233#issuecomment-300943889
         driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", password), encrypted=False)
         session = driver.session()
 
+        # Password handling taken from https://github.com/robinedwards/django-neomodel
         try:
             result = session.run("MATCH (a:Person) WHERE a.name = {name} "
                                 "RETURN a.name AS name, a.title AS title",
                                 {"name": "Crick"})
         except CypherError as ce:
-            # handle instance without password being changed
             if 'The credentials you provided were valid, but must be changed before you can use this instance' in str(ce):
                 new_password = 'test'
                 session.run("CALL dbms.changePassword({password})", {'password': new_password})
@@ -43,22 +39,22 @@ class Neo4jTestCase(unittest.TestCase):
             else:
                 raise ce
 
-            # run the test
-            session.run("CREATE (a:Person {name: {name}, title: {title}})",
-                        {"name": "Crick", "title": "Professor"})
-            result = session.run("MATCH (a:Person) WHERE a.name = {name} "
-                                "RETURN a.name AS name, a.title AS title",
-                                {"name": "Crick"})
-            for record in result:
-                print("%s %s" % (record["title"], record["name"]))
-            assert record["title"] == "Professor"
-            assert record["name"] == "Crick"
+        # run the test
+        session.run("CREATE (a:Person {name: {name}, title: {title}})",
+                    {"name": "Crick", "title": "Professor"})
+        result = session.run("MATCH (a:Person) WHERE a.name = {name} "
+                            "RETURN a.name AS name, a.title AS title",
+                            {"name": "Crick"})
+        for record in result:
+            print("%s %s" % (record["title"], record["name"]))
+        assert record["title"] == "Professor"
+        assert record["name"] == "Crick"
 
-            # clean up
-            session.run("MATCH (a:Person) WHERE a.name = {name} "
-                        "DETACH DELETE a",
-                        {"name": "Crick"})
-            session.close()
+        # clean up
+        session.run("MATCH (a:Person) WHERE a.name = {name} "
+                    "DETACH DELETE a",
+                    {"name": "Crick"})
+        session.close()
 
 if __name__ == '__main__':
     unittest.main()
